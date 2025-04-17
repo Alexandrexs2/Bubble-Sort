c338c7372edfb02e325ca345d21f7e9c31839baf
Alexandrexs2 <xs2.alexandre@uni9.edu.br>
2025-04-16 22:03:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 let dados = [97, 45, 49, 32, 2, 5, 9, 99, 1, 7];
 
-for (let i = 0; i < dados.length; i++) {
-    console.log("O ìndice é: " + i + "O valor é: " + dados[i] );
-}
+let ordenado = false
+do 
+{
+    for (let i = 0; i < dados.length; i++) {    
+        let aux = 0;
+        if(dados[i] > dados[i + 1]){
+            console.log(`$[${dados[i]}] é maior que $[${dados[i + 1]}]. Realizando inversão`);
+            aux = dados[i];
+            dados[i] = dados[i + 1];
+            dados[i + 1] = aux;
+        }  
+        console.table(dados);
+    }
+}while(!ordenado);
 
-console.log(dados);
+console.log(dados);let dados = [97, 45, 49, 32, 2, 5, 9, 99, 1, 7];
+
