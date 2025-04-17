321e675afe6c42f8affa0733931e0b174c6dcb00
Alexandrexs2 <xs2.alexandre@uni9.edu.br>
2025-04-17 00:32:47 +0000

    Bubble Sort

---
+++ main.js
@@ -0,0 +1,7 @@
+let dados = [97, 45, 49, 32, 2, 5, 9, 99, 1, 7];
+
+for (let i = 0; i < dados.length; i++) {
+    console.log("O ìndice é: " + i + "O valor é: " + dados[i] );
+}
+
+console.log(dados);
